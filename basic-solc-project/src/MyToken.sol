17e73c402a4631e80f0882c1a8d62070af81fa5f
Aman kumar <aman9693kumar@gmail.com>
2024-02-10 10:44:00 +0530

    password audit

---
+++ basic-solc-project/src/MyToken.sol
@@ -0,0 +1,10 @@
+// SPDX-Licesnse-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20{
+    constructor() ERC20("MyTokenName", "MTN") {      
+    }
+}
